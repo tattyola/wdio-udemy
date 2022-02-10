cc71841519aeb26d84e2a384f505326a2d1cf693
Tatsiana Astrouskaya <82601614+tattyola@users.noreply.github.com>
2022-02-09 23:25:36 -0500

    create loginData.json (#2)
    create e2eData.json

--- test/specs/poFirstTest.js
+++ test/specs/poFirstTest.js
@@ -1,6 +1,27 @@
 const loginPage = require('../pageObjects/loginPage')
+const fs = require('fs')
+let credentials = JSON.parse(fs.readFileSync('test/specs/testData/loginData.json'))
 
 describe('Ecomerce application', function () {
+
+    credentials.forEach(({username,password}) => {
+        it('Login fail', function () {
+            browser.url('https://rahulshettyacademy.com/loginpagePractise/#');
+            console.log(browser.getTitle());
+            browser.pause(3000);
+            expect(browser).toHaveTitleContaining('LoginPage Practise | Rahul Shetty Academy')
+
+            loginPage.Login(username, password)
+
+            console.log(loginPage.alert.getText() + ' is the text I am seeing on the screen')
+            browser.waitUntil( () =>
+                loginPage.signIn.getAttribute('value') === 'Sign In', {timeout: 5000, timeoutMsg: 'Error message did not appear '})
+            console.log(loginPage.alert.getText() + ' is the text I am seeing on the screen')
+            console.log(loginPage.text.getText())
+            expect(loginPage.text).toHaveTextContaining('(username is rahulshettyacademy and Password is learning)')
+        });
+    })
+
     it('Login success', function () {
         browser.url('https://rahulshettyacademy.com/loginpagePractise/#');
         console.log(browser.getTitle());
@@ -13,22 +34,5 @@ describe('Ecomerce application', function () {
         console.log(browser.getTitle());
         expect(browser).toHaveTitleContaining('ProtoCommerce')
         expect(browser).toHaveUrlContaining('shop')
-    });
-
-    it('Login fail', function () {
-        browser.url('https://rahulshettyacademy.com/loginpagePractise/#');
-        console.log(browser.getTitle());
-        browser.pause(3000);
-        expect(browser).toHaveTitleContaining('LoginPage Practise | Rahul Shetty Academy')
-
-        loginPage.Login('rahulshettyacademy', 'learning111')
-
-        console.log(loginPage.alert.getText() + ' is the text I am seeing on the screen')
-        browser.waitUntil( () =>
-            loginPage.signIn.getAttribute('value') === 'Sign In', {timeout: 5000, timeoutMsg: 'Error message did not appear '})
-        console.log(loginPage.alert.getText() + ' is the text I am seeing on the screen')
-        console.log(loginPage.text.getText())
-        expect(loginPage.text).toHaveTextContaining('(username is rahulshettyacademy and Password is learning)')
-
     });
 });
